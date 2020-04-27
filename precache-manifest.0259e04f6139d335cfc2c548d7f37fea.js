e51d30ee467ff783fd51d865a87a9ff49b36c710
Cathy <cathy.healey@gmail.com>
2020-04-27 11:22:33 -0400

    Updates

---
+++ precache-manifest.0259e04f6139d335cfc2c548d7f37fea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bbf81a779fe9af5d419c7481616dcd",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "1ef796b9e282112bb554",
+    "url": "/portfolio-react/static/css/main.ef031916.chunk.css"
+  },
+  {
+    "revision": "1fdd4f4dffcef201b909",
+    "url": "/portfolio-react/static/js/2.13fcd782.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.13fcd782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef796b9e282112bb554",
+    "url": "/portfolio-react/static/js/main.3845f7df.chunk.js"
+  },
+  {
+    "revision": "ded5e5aa532cde99b12e",
+    "url": "/portfolio-react/static/js/runtime-main.dfdf69bc.js"
+  },
+  {
+    "revision": "f628e4f5e61fcda91887dc43886e9cdf",
+    "url": "/portfolio-react/static/media/nat-10-small.f628e4f5.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/portfolio-react/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "00cad672177e3c5b1668cca19beda156",
+    "url": "/portfolio-react/static/media/nat-5-small.00cad672.jpg"
+  },
+  {
+    "revision": "6fde27bb969f3639461637c42ca9ea90",
+    "url": "/portfolio-react/static/media/nat-6-small.6fde27bb.jpg"
+  },
+  {
+    "revision": "9ed3181c12ecb499300ff8ba74385715",
+    "url": "/portfolio-react/static/media/nat-7-small.9ed3181c.jpg"
+  }
+]);
